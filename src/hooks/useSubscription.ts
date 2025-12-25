6eac158e0c1c45921ee3eb87c536efcb6e2a44bb
Sam <sam.vetiveria@gmail.com>
2025-12-25 15:42:29 -0800

    Fix store import path

--- src/hooks/useSubscription.ts
+++ src/hooks/useSubscription.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabase';
-import { useStore } from '../store';
+import { useStore } from '../store/useStore';
 
 interface SubscriptionStatus {
   canCreateProjects: boolean;
